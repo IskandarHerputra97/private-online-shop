072e49a58bdfa873721ec38177cfea3849e18a6f
Iskandar Herputra Wahidiyat <iskandarherputra@gmail.com>
2021-05-28 22:32:40 +0700

    Setup HomeViewController basic layout.

--- OnlineShopping/HomeViewController.swift
+++ OnlineShopping/HomeViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var headerImageView: UIImageView!
+    @IBOutlet weak var itemCategoryTableview: UITableView!
+    @IBOutlet weak var monthlyPromoLabel: UILabel!
+    @IBOutlet weak var promoItemTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
