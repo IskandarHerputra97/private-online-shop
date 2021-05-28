dddfb33b1e90e1674241177fcc439f32b5f96bd8
Iskandar Herputra Wahidiyat <iskandarherputra@gmail.com>
2021-05-28 20:51:24 +0700

    Basic setup.

--- OnlineShopping/AppDelegate.swift
+++ OnlineShopping/AppDelegate.swift
@@ -9,28 +9,29 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        showMainViewController()
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    //MARK: - Setup
+    @objc private func showMainViewController() {
+        if self.window == nil {
+            self.window = UIWindow()
+        }
+        
+        let landingPageVC: UIViewController = createNonLoginLandingPage()
+        
+        self.window?.rootViewController = landingPageVC
+        self.window?.makeKeyAndVisible()
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    private func createNonLoginLandingPage() -> UIViewController {
+        let landingPageViewController: UIViewController = HomeViewController()
+        let navController: UINavigationController = UINavigationController(rootViewController: landingPageViewController)
+        return navController
     }
-
-
 }
 
---
+++ OnlineShopping/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  OnlineShopping
+//
+//  Created by Iskandar Herputra Wahidiyat on 28/05/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    //MARK: - Setup
+    private func setupView() {
+        title = "APLIKASI BELANJA ONLINE"
+    }
+}
